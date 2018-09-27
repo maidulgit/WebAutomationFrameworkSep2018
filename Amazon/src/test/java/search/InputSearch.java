cebaffc86043cbfd57e10e46e86769e0173da817
maidulgit <maidul360@gmail.com>
2018-09-27 03:52:58 -0400

    Basic framework created following the class lecture

---
+++ Amazon/src/test/java/search/InputSearch.java
@@ -0,0 +1,13 @@
+package search;
+
+import base.CommonAPI;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+public class InputSearch extends CommonAPI {
+    @Test
+            public void search(){
+            driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Laptop", Keys.ENTER);
+}
+}
