db89dc4b9af93f567a3a50967cd596c0993f2229
maidulgit <maidul360@gmail.com>
2018-09-30 14:19:27 -0400

    LoginPage and LoginPage Test updated

--- Amazon/src/main/java/pages/LogInPage.java
+++ Amazon/src/main/java/pages/LogInPage.java
@@ -1,4 +1,57 @@
 package pages;
 
-public class LogInPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import java.io.IOException;
+
+public class LogInPage{
+    @FindBy(how = How.ID, using ="nav-link-accountList")
+    public static WebElement signInWebElement;
+
+    @FindBy(how = How.ID, using ="ap_email")
+    public static WebElement emailBoxWebElement;
+
+    @FindBy(how = How.ID, using ="continue")
+    public static WebElement continueButtonWebElement;
+
+    @FindBy(how = How.CLASS_NAME, using ="a-expander-prompt")
+    public static WebElement needHelpWebElement;
+
+    @FindBy(how = How.ID, using ="createAccountSubmit")
+    public static WebElement createAccountWebElement;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"auth-email-missing-alert\"]/div/div")
+    public static WebElement missingEmailErrorWebElement;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"auth-error-message-box\"]/div/div/ul/li/span")
+    public static WebElement invalidEmailErrorWebElement;
+
+    // Sign in with empty email box
+    public String signInNull() throws InterruptedException {
+        signInWebElement.click();
+        emailBoxWebElement.sendKeys("");
+        continueButtonWebElement.click();
+        String errorMessage = missingEmailErrorWebElement.getText();
+        System.out.println("Sign In With No Email Error Message" + errorMessage);
+        return errorMessage;
+    }
+
+    // Sign in with valid email address
+    public void signInValid() throws InterruptedException {
+        signInWebElement.click();
+        emailBoxWebElement.sendKeys("cent_mercy@yahoo.com");
+        continueButtonWebElement.click();
+    }
+
+    // Sign in with invalid email address
+    public String signInInvalid() throws InterruptedException {
+        signInWebElement.click();
+        emailBoxWebElement.sendKeys("cent_mercy@gmail.com");
+        continueButtonWebElement.click();
+        String errorMessage2 = invalidEmailErrorWebElement.getText();
+        System.out.println("Sign In With Invalid Email Error Message" + errorMessage2);
+        return errorMessage2;
+    }
 }
--- Amazon/src/test/java/search/TestLoginPage.java
+++ Amazon/src/test/java/search/TestLoginPage.java
@@ -1,4 +1,30 @@
 package search;
 
-public class TestLoginPage {
+import base.CommonAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import pages.LogInPage;
+
+import java.io.IOException;
+
+public class TestLoginPage extends CommonAPI {
+
+    @Test
+    public void emptyData()throws InterruptedException {
+        LogInPage logInPage = PageFactory.initElements(driver, LogInPage.class);
+        logInPage.signInNull();
+
+    }
+
+    @Test
+    public void validEmailData() throws InterruptedException {
+        LogInPage validEmail = PageFactory.initElements(driver, LogInPage.class);
+        validEmail.signInValid();
+    }
+
+    @Test
+    public void invalidEmailData()throws InterruptedException {
+        LogInPage invalidEmail = PageFactory.initElements(driver, LogInPage.class);
+        invalidEmail.signInInvalid();
+    }
 }
