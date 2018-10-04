7c6ab79e3e04c997237e223b20393c8593f006bd
maidulgit <maidul360@gmail.com>
2018-10-03 21:31:54 -0400

    Extent Report Added for Amazon.pages.HomaPage

--- Amazon/src/main/java/pages/HomePage.java
+++ Amazon/src/main/java/pages/HomePage.java
@@ -1,9 +1,11 @@
 package pages;
 
+import base.CommonAPI;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import reporting.TestLogger;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -19,51 +21,52 @@ public class HomePage {
     public static WebElement submitButtonWebElement;
 
     public WebElement getSearchInputWebElement() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         return searchInputWebElement;
     }
-
     public WebElement getSubmitButtonWebElement() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         return submitButtonWebElement;
     }
-
     public void searchFor(String value){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         getSearchInputWebElement().sendKeys(value);
     }
     public void submitSearchButton(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         getSubmitButtonWebElement().click();
     }
     public void clearInput(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         getSearchInputWebElement().clear();
     }
-
     public void searchItemsAndSubmitButton()throws IOException {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         List<String> list = getItemValue();
         for(int i=0; i<list.size(); i++) {
             searchFor(list.get(i));
             submitSearchButton();
-            //validate books data
             clearInput();
         }
     }
-
     public WebElement getSearchInputField() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         return searchInputWebElement;
     }
-
     public void setSearchInputField(WebElement searchInputField) {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         this.searchInputWebElement = searchInputField;
     }
-
     public void searchItems()throws InterruptedException{
-
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         List<String> itemList = getItemValue();
         for(String st: itemList) {
             getSearchInputField().sendKeys(st, Keys.ENTER);
             getSearchInputField().clear();
         }
     }
-
     public List<String> getItemValue(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         List<String> itemsList = new ArrayList<String>();
         itemsList.add("Java Book");
         itemsList.add("Selenium Book");
@@ -73,8 +76,6 @@ public class HomePage {
         itemsList.add("ear-ring");
         itemsList.add("ps4games");
         itemsList.add("macAir");
-
         return itemsList;
     }
-
 }
--- Amazon/src/test/java/search/TestHomePage.java
+++ Amazon/src/test/java/search/TestHomePage.java
@@ -4,6 +4,7 @@ import base.CommonAPI;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import reporting.TestLogger;
 
 import java.io.IOException;
 
@@ -11,6 +12,7 @@ public class TestHomePage extends CommonAPI {
 
     @Test
     public void searchData()throws IOException{
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         HomePage homePage = PageFactory.initElements(driver, HomePage.class);
         homePage.searchItemsAndSubmitButton();
     }
